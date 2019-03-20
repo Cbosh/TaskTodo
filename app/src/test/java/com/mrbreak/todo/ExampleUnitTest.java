0b06d1a3deb1e7292d1b605eed46f320f886e00a
Administrator <administrator@Administrators-MacBook-Pro.local>
2019-03-20 20:49:42 +0200

    private policy update

--- app/src/test/java/com/mrbreak/todo/ExampleUnitTest.java
+++ app/src/test/java/com/mrbreak/todo/ExampleUnitTest.java
@@ -1,6 +1,14 @@
 package com.mrbreak.todo;
 
+import android.app.Application;
+
+import com.mrbreak.todo.repository.model.ToDoModel;
+import com.mrbreak.todo.util.Utils;
+import com.mrbreak.todo.viewmodel.ToDoListViewModel;
+
 import org.junit.Test;
+
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +18,20 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void given_todo_check_if_its_over_due() {
+        ToDoModel toDoModel = new ToDoModel();
+        toDoModel.setDone(false);
+        toDoModel.setStartTime("");
+        toDoModel.setContent("Read");
+        //toDoModel.setCreatedDate(Utils.getDueDate(new Date());
+        ToDoListViewModel viewModel = new ToDoListViewModel(new Application());
+        assertEquals(toDoModel.getContent(), "Read");
     }
 }
